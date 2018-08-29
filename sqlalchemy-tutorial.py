f57328a7e5643b037f62f4cbd308a9df01346cc5
shyam <shyamp@xhsip.in>
2018-08-29 09:31:13 +0530

    second commit

--- sqlalchemy-tutorial.py
+++ sqlalchemy-tutorial.py
@@ -15,4 +15,51 @@ addresses = Table('addresses', metadata,
                   Column('email_address', String, nullable=False)
 )
 
-print(users.metadata)
+#print(users.metadata)
+metadata.create_all(engine)
+
+ins = users.insert().values(name='jack', fullname='jack jones')
+
+conn = engine.connect()
+conn.execute(ins)
+
+ins = users.insert()
+conn.execute(ins, id=2, name='wendy', fullname='Wendy Williams')
+
+conn.execute(addresses.insert(), [
+    {'user_id' : 1, 'email_address' : 'jack@yahoo.com'},
+    {'user_id' : 1, 'email_address' : 'jack@msn.com'},
+    {'user_id' : 2, 'email_address' : 'www@sss.org'},
+    {'user_id' : 2, 'email_address' : 'wendy@aol.com'},
+])
+
+from sqlalchemy.sql import select
+s = select([users])
+result = conn.execute(s)
+
+for row in conn.execute(s):
+    print("name:", row[users.c.name], "; fullname:", row[users.c.fullname])
+
+s = select([users.c.name, users.c.fullname])
+result = conn.execute(s)
+for row in result:
+    print(row)
+
+s = select([users, addresses]).where(users.c.id == addresses.c.user_id)
+for row in conn.execute(s):
+    print(row)
+
+from sqlalchemy.sql import and_, or_, not_
+print(and_(
+    users.c.name.like('j%'),
+    users.c.id == addresses.c.user_id,
+    or_(
+        addresses.c.email_address == 'wendy@aol.com',
+        addresses.c.email_address == 'jack@yahoo.com'
+        ),
+    not_(users.c.id > 5)
+    )
+)
+
+result.close()
+
