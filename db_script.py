2a79f2789e5d9809ad4d0d87be4f55d6e092fe84
shyam <shyamp@xhsip.in>
2018-09-19 10:17:11 +0530

    "table dictionaries and csv file dictionaries made"

--- db_script.py
+++ db_script.py
@@ -1,4 +1,5 @@
 from sqlalchemy import engine, MetaData, select, Table, create_engine
+from sqlalchemy.sql import text
 import os
 import csv
 import glob
@@ -9,34 +10,52 @@ conn = engine.connect()
 
 tablemeta = MetaData(bind=engine, reflect=True)
 
-#with open('/home/shyam/project/code/practice/mtm-data/Sea-Trial-Data.csv', \
+# with open('/home/shyam/project/code/practice/mtm-data/Sea-Trial-Data.csv', \
 #          newline='') as csv_file:
 #    csv_data = csv.reader(csv_file, delimiter=',')
 #    for row in csv_data:
 #        # conn.execute(table_sea_trial.insert().values(row))
 #        print(row)
-#table_sea_trial = Table('SEATRIAL', tablemeta, autoload=True, autoload_with=engine)
+# table_sea_trial = Table('SEATRIAL', tablemeta, autoload=True, autoload_with=engine)
+
 
 csv_dict = {}
 table_dict = {}
-for csvfile in glob.glob(os.path.join('./mtm-data', '*.csv')):
+for csvfile in glob.glob(os.path.join('../database/mtm-data', '*.csv')):
     with open(csvfile) as f:
         csv_data = csv.reader(f, delimiter=',')
         row1 = next(csv_data)
-        #print('\n', csvfile)
-        #print(row1)
+        # print('\n', csvfile)
+        # print(row1)
         
-        #pattern = re.compile('\.\/mtm\-data[\S]*\.csv')
-        pattern1 = '\.\/mtm\-data\/'
+        # pattern = re.compile('\.\/mtm\-data[\S]*\.csv')
+        pattern1 = '\.\.\/database\/mtm\-data\/'
         pattern2 = '\.csv'
         stripped_first = re.sub(pattern1, '', csvfile)
         stripped_file_name = re.sub(pattern2, '', stripped_first)
-        #pattern.match(csvfile).group()
+        # pattern.match(csvfile).group()
         csv_dict[stripped_file_name.lower()] = [row.lower() for row in row1]
 
-print(csv_dict)
+# print(csv_dict)
+
+# for t in tablemeta.sorted_tables:
+    # print('*'*10, '\n', t.name, '\n', '*'*10)
+    # for c in t.c:
+    #     print('\n', c)
+
 
 for t in tablemeta.sorted_tables:
-    print('*'*10, '\n', t.name, '\n', '*'*10)
-    for c in t.c:
-        print('\n', c)
+    # print(t, type(t))
+    col_names = [str(col).lower() for col in t.c.keys()]
+    # print(str(t.name), col_names)
+    table_name = str(t.name).lower()
+    table_dict[table_name] = col_names
+    # print(t.select(), '***', type(t.select()))
+    # table_dict[t.name] = [dict(row) for row in engine.execute(t.select())]
+#    statement = text("describe ")
+#    conn.execute(statement)
+# print(table_dict)
+
+for file_name in csv_dict.keys():
+    if not file_name in table_dict.keys():
+        print(file_name)
